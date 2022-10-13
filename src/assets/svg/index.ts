cd2f72820357f2db799ee963c9a61deeda6262d1
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-10-13 16:45:29 -0300

    added: export icons

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,31 @@
+import Acceleration from './acceleration.svg';
+import Arrow from './arrow.svg';
+import Brand from './brand.svg';
+import Car from './car.svg';
+import Done from './done.svg';
+import Energy from './energy.svg';
+import Exchange from './exchange.svg';
+import Force from './force.svg';
+import Gasoline from './gasoline.svg';
+import Hybrid from './hybrid.svg';
+import LogoBackgroundGray from './logo_background_gray.svg';
+import Logo from './logo.svg';
+import People from './people.svg';
+import Speed from './speed.svg';
+
+export {
+  Acceleration,
+  Arrow,
+  Brand,
+  Car,
+  Done,
+  Energy,
+  Exchange,
+  Force,
+  Gasoline,
+  Hybrid,
+  LogoBackgroundGray,
+  Logo,
+  People,
+  Speed,
+};
