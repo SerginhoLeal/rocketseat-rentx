d40fad9bbcd7c80b41053e449a9ba2c09bf6afac
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-10-13 16:37:35 -0300

    added: import text fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import * as Native from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
+import { useFonts } from 'expo-font';
+import { ThemeProvider } from 'styled-components';
+
+import HomeScreen from './src/screens/home';
+import DetailsScreen from './src/screens/carDetails';
+import SchedulingScreen from './src/screens/scheduling';
+
+import theme from './src/styles/theme';
+
+SplashScreen.preventAutoHideAsync();
+
+const Main: React.FC = () => {
+  const [fontsLoading] = useFonts({
+    Inter_400Regular: require('./src/assets/fonts/Inter-Regular.ttf'),
+    Inter_500Medium: require('./src/assets/fonts/Inter-Medium.ttf'),
+    Archivo_400Regular: require('./src/assets/fonts/Archivo-Regular.ttf'),
+    Archivo_500Medium: require('./src/assets/fonts/Archivo-Medium.ttf'),
+    Archivo_600SemiBold: require('./src/assets/fonts/Archivo-SemiBold.ttf'),
+  });
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (fontsLoading) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoading]);
+
+  if (!fontsLoading) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Native.SafeAreaView onLayout={onLayoutRootView}>
+      <ThemeProvider theme={theme}>
+        <SchedulingScreen />
+      </ThemeProvider>
+    </Native.SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Main;
