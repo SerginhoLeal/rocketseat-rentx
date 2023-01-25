bb224a1aad2486be1532605673ea0e73a16079d8
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-01-25 14:52:44 -0300

    created: dtos

---
+++ src/dtos/routes.ts
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  HomeScreen: undefined;
+  MyCarsScreen: undefined;
+  CarDetailsScreen: undefined;
+  SchedulingScreen: undefined;
+  SchedulingDetailsScreen: undefined;
+  SchedulingCompleteScreen: undefined;
+};
+
+export type RoutesNameProps = {
+  myCars: 'MyCarsScreen';
+  home: 'HomeScreen';
+  carDetails: 'CarDetailsScreen';
+  scheduling: 'SchedulingScreen';
+  schedulingDetails: 'SchedulingDetailsScreen';
+  schedulingComplete: 'SchedulingCompleteScreen';
+};
