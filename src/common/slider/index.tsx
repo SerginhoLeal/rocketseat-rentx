1a3ecc855140421aea386f5ff285221002b60201
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-01-25 16:07:54 -0300

    modification: Getting the image in focus

--- src/common/slider/index.tsx
+++ src/common/slider/index.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
+import { FlatList, ViewToken } from 'react-native';
 import * as Styles from './styles';
 
 interface Props {
   imageUrl: string[];
 };
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+};
+
 export const Slider: React.FC<Props> = ({ imageUrl }: Props) => {
+  const [imageIndex, setImageIndex] = React.useState(0);
+
+  const IndexChange = React.useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!;
+    setImageIndex(index)
+  });
+
   return (
     <Styles.Container>
       <Styles.ImageIndexes>
-        <Styles.ImageIndex active={true} />
-        <Styles.ImageIndex active={false} />
-        <Styles.ImageIndex active={false} />
-        <Styles.ImageIndex active={false} />
+        {imageUrl.map((_, index) => <Styles.ImageIndex key={String(index)} active={index === imageIndex} />)}
       </Styles.ImageIndexes>
 
-      <Styles.CarImageWrapper>
-        <Styles.CarImage
-          source={{ uri: imageUrl[0] }}
-          resizeMode='contain'
-        />
-      </Styles.CarImageWrapper>
+      <FlatList
+        data={imageUrl}
+        keyExtractor={key => key}
+        renderItem={({ item }) => (
+          <Styles.CarImageWrapper>
+            <Styles.CarImage
+              source={{ uri: item }}
+              resizeMode='contain'
+            />
+          </Styles.CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={IndexChange.current}
+      />
     </Styles.Container>
   );
 };
